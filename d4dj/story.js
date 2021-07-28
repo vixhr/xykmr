c3cf0484843cfb8a4d3f13011332e2ebe700c368
eclair <87921261+vixhr@users.noreply.github.com>
2021-07-27 18:03:01 -0700

    Update story.js

--- d4dj/story.js
+++ d4dj/story.js
@@ -1,6 +1,8 @@
-                
- $( document ).ready(function() {
+$( document ).ready(function() {
     mw.loader.using('mediawiki.api', function() {
         $('body[class*="Transcripts"] .portable-infobox').addClass('story-transcript');
     });
+   $('.story-transcript .portable-infobox:first-child > .pi-item .pi-data .pi-item-spacing > *:nth-child(1)').after(`
+<h2 class="pi-item pi-item-spacing pi-title pi-secondary-background" data-source="episode">Story</h2>
+`);
 });
