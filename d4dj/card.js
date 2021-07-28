5fa58c5eff3fbd48f6ec6cba2fec52101bcff37c
eclair <87921261+vixhr@users.noreply.github.com>
2021-07-28 13:28:42 -0700

    Update and rename story.js to card.js

---
+++ d4dj/card.js
@@ -0,0 +1,28 @@
+$( document ).ready(function() {
+    mw.loader.using('mediawiki.api', function() 
+       addCategories();
+
+        $('body[class*="Cards"] .quote').addClass('cardQuote');
+});
+
+// addcategories - code creds jeaoq(kyuu)
+function addCategories(){
+    (function($, mw) {
+    var fn = function() {
+        var cats = mw.config.get('wgCategories'), newClasses;
+        if (cats) {
+        newClasses = $.map(cats, function(el) {
+            return 'cat-' + encodeURIComponent(el.replace(/[ .]/g, '_')).replace(/%/g, '_');
+        }).join(' ');
+        $(document.body).addClass(newClasses);
+        // console.log(newClasses);
+        }
+    };
+    if (document.body) {
+        fn();
+    } else {
+        $(fn);
+    }
+    })(jQuery, mw);
+
+}
--- d4dj/story.js
+++
@@ -1,8 +0,0 @@
-$( document ).ready(function() {
-    mw.loader.using('mediawiki.api', function() {
-        $('body[class*="Transcripts"] .portable-infobox').addClass('story-transcript');
-    });
-   $('.story-transcript .portable-infobox:first-child > .pi-item .pi-data .pi-item-spacing > *:nth-child(1)').after(`
-<h2 class="pi-item pi-item-spacing pi-title pi-secondary-background" data-source="episode">Story</h2>
-`);
-});
